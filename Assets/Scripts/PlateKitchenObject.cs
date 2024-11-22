a871590536a13338d1e93962dd989d01686f04bd
silviudbv <silviu.dobrik@gmail.com>
2024-11-22 20:26:37 +0200

    Added visuals to picking up the kitchen objects with plate, first burger done

--- Assets/Scripts/PlateKitchenObject.cs
+++ Assets/Scripts/PlateKitchenObject.cs
@@ -1,8 +1,16 @@
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class PlateKitchenObject : KitchenObject
 {
+    public event EventHandler<OnIngredientAddedEventArgs> OnIngredientAdded;
+
+    public class OnIngredientAddedEventArgs: EventArgs
+    {
+        public KitchenObjectSO kitchenObjectSO;
+    }
+
     [SerializeField]
     private List<KitchenObjectSO> validKitchenObjectSOList;
 
@@ -26,6 +34,12 @@ public class PlateKitchenObject : KitchenObject
         }
 
         kitchenObjectSOList.Add(kitchenObjectSO);
+
+        OnIngredientAdded?.Invoke(this, new OnIngredientAddedEventArgs
+        {
+            kitchenObjectSO = kitchenObjectSO
+        });
+
         return true;
     }
 }
